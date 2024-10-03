0de6259c399bed46bd66480e96e5806d51bf9c70
frabe1579 <francesco@totalprogrammi.it>
2024-10-03 17:57:22 +0200

    Primo esempio base con barman

---
+++ 00_init.sql
@@ -0,0 +1,17 @@
+CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
+SELECT pg_create_physical_replication_slot('replication_slot');
+
+CREATE USER barman SUPERUSER PASSWORD 'barman';
+CREATE USER streaming_barman WITH REPLICATION ENCRYPTED PASSWORD 'stm_barman_pw';
+SELECT pg_create_physical_replication_slot('barman');
+-- GRANT EXECUTE ON FUNCTION pg_backup_start(text, boolean) to barman;
+-- GRANT EXECUTE ON FUNCTION pg_backup_stop(boolean) to barman;
+GRANT EXECUTE ON FUNCTION pg_start_backup(text, boolean, boolean) to barman;
+GRANT EXECUTE ON FUNCTION pg_stop_backup() to barman;
+GRANT EXECUTE ON FUNCTION pg_stop_backup(boolean, boolean) to barman;
+GRANT EXECUTE ON FUNCTION pg_switch_wal() to barman;
+GRANT EXECUTE ON FUNCTION pg_create_restore_point(text) to barman;
+
+GRANT pg_read_all_settings TO barman;
+GRANT pg_read_all_stats TO barman;
+GRANT pg_checkpoint TO barman;
---
+++ README.md
@@ -0,0 +1,9 @@
+# Installare barman
+
+```
+sudo apt install -y postgresql-common
+sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+apt update
+apt install postgresql-client-14
+apt install barman
+```
